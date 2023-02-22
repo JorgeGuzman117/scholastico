6777385aabf54f25bc94ff2d3216c318a5f703d0
JorgeGuzman117 <123588203+JorgeGuzman117@users.noreply.github.com>
2023-02-22 15:56:26 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*=============== MAIN DIV LOADER ===============*/
-window.addEventListener("DOMContentLoaded", () => {
+window.onload = function() {
+// window.addEventListener("DOMContentLoaded", () => {
 // $(document).ready(function () {
 	let bodyy = document.querySelector("body"); // First hide main scroll to avoid conflicts in preloader & begining video
 	bodyy.classList.add("hide-scroll");
